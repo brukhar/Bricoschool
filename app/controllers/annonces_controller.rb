fcce26b6f8f8ad8f150f605c2d16aab7fbcc50a6
brukhar <objecris@gmail.com>
2017-05-14 18:16:40 +0200

    Ajout fonction supprimer et Editer pour les annonces

--- app/controllers/annonces_controller.rb
+++ app/controllers/annonces_controller.rb
@@ -7,6 +7,17 @@ class AnnoncesController < ApplicationController
     @annonce = Annonce.find(params[:id])
   end
 
+  def edit
+    @annonce = Annonce.find(params[:id])
+  end
+
+  def update
+    @annonce = Annonce.find(params[:id])
+    @annonce.update_attributes(params.require(:annonce).permit(:titre, :descr))
+
+    redirect_to @annonce
+  end
+
   def new
   end
 
@@ -15,5 +26,10 @@ class AnnoncesController < ApplicationController
 
     @annonce.save
     redirect_to @annonce
+  end
+
+  def destroy
+    Annonce.find(params[:id]).destroy
+    redirect_to Annonce
   end
 end
